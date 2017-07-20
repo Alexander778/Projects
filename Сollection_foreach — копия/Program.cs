fd16bdfb520fe43bafb133a067cecb61db88b486
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-07-20 15:15:55 +0300

    no message

---
+++ Сollection_foreach — копия/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection_foreach
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+
+			List<int> numbers = new List<int>() { };
+
+			for (int i = 0; i < 10001; i++)
+			{
+				numbers.Add(i);
+			}
+
+			DateTime dold = DateTime.Now;
+			foreach (int i in numbers)
+			{
+				Console.WriteLine(i);
+			}
+            TimeSpan sp = DateTime.Now - dold;
+			Console.WriteLine("Время выполнения кода "+sp);
+			Console.ReadKey();
+		}
+	}
+}
