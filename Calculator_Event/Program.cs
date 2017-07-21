3c6aa00e8a6979f83ad5fe6966f71cf2f8f8dceb
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-07-21 12:57:29 +0300

    no message

---
+++ Calculator_Event/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Calculator_Event
+{
+    public class Calculator
+    {
+
+        public delegate void History();
+
+        public event History OnHistory;
+
+        public  void Sum()
+        {
+            int x = 10;
+            int y = 20;
+            int result = x + y;
+            OnHistory();
+
+        }
+
+
+    }
+
+    class Handler_I
+    {
+        public void Message()
+        {
+            Console.WriteLine("10+20=30");
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Calculator Suma = new Calculator();
+            Handler_I Handler1 = new Handler_I();
+            Suma.OnHistory += Handler1.Message;
+
+
+          
+          
+
+        }
+    }
+}
