1126e8f33f263b05643d7376d16411c558ede69d
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-07-25 16:07:21 +0300

    no message

---
+++ Event_Training_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Event_Training_2
+{
+    public class Button
+    {
+        public delegate void ClickButton(string N,string D);//делегат
+        public event ClickButton OnClick;//событие
+        public string ShowNameOfButton(string Name,string Date)//метод
+        {
+            string result = Name + Date;
+            if(OnClick!=null) //проверка
+            {
+                OnClick(Name,Date);
+            }
+            return result ;
+        }
+    }
+
+
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10;i++) //цикл
+            {
+                Button button = new Button();
+             
+                button.OnClick += ShowInfoButton; //подписка на событие
+                button.ShowNameOfButton("Button"+i, "25.07.2017");
+
+            }
+        }
+        public static void ShowInfoButton(string Name,string Date) //обработчик
+        {
+            Console.WriteLine("{0} {1} is clicked",Date,Name);
+        }
+
+    }
+}
