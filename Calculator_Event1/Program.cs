035f516b070d9e3e1b2937a94d8b00ce988b513d
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-07-25 10:40:29 +0300

    no message

--- Calculator_Event1/Program.cs
+++ Calculator_Event1/Program.cs
@@ -20,33 +20,36 @@ namespace Calculator_Event
         }
 
         //1. остальные методы нужно переписать также как и Sum
-        /*   public double Raz (double x, double y)
+          public double Raz (double x, double y)
            {
+            var result = x - y;
                if(OnHistory !=null)
                {
-                   OnHistory(x, y, "-");
+                OnHistory(x, y, "-",result);
                }
-               return x - y;
+               return result;
 
            }
            public double Umnog(double x, double y)
            {
+            var result = x * y;
                if(OnHistory!=null)
                {
-                   OnHistory(x, y, "*");
+                OnHistory(x, y, "*",result);
                }
-               return x * y;
+            return result;
 
            }
            public double Delen(double x,double y)
            {
+            var result = x / y;
                if(OnHistory!=null)
                {
-                   OnHistory(x, y, "/");
+                   OnHistory(x, y, "/",result);
                }
-               return x / y;
+               return result;
 
-           }*/
+           }
 
         //!!!метод обработчик не может лежать в том же классе что и основной функционал
         //!!!в том то и суть обработчиков, мы можем подписаться на какое-то событие чужого класса
@@ -60,25 +63,18 @@ namespace Calculator_Event
             //создаем экземпляр калькулятора, достаточно одного для тестирования всех операций
             Calculator myCalculator = new Calculator();
 
-            //Изменения тут 
-            // Это неверно - комментирую
-            /*myCalculator.HistoryMessage(15, 26, "+",myCalculator.Sum(15,26));
-            myCalculator.HistoryMessage(78, 56, "-", myCalculator.Raz(78, 56));
-            myCalculator.HistoryMessage(2, 2, "*", myCalculator.Umnog(2, 2));
-            myCalculator.HistoryMessage(80, 5, "/", myCalculator.Delen(80, 5));*/
-            //
+
 
             //3. тут назначаем обработчик OnHistory который будет выводить историю
             //!!! Это должно быть обязательно, иначе события не будут обрабатываться, нету обработчика а мы проверяем его наличие в коде  if (OnHistory != null)
             myCalculator.OnHistory += HistoryMessage;
 
 
-            //Я убрал эти методы, так как они считают цифры непосредственно в вызове события.Можно так делать? 
-            //зря убрал
+
             myCalculator.Sum(15, 26);
-            /*   myCalculator.Delen(80, 5);
-               myCalculator.Raz(78, 56);
-               myCalculator.Umnog(2, 2);*/
+            myCalculator.Delen(80, 5);
+            myCalculator.Raz(78, 56);
+            myCalculator.Umnog(2, 2);
 
             Console.ReadKey();
         }
