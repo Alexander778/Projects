76ad44115abc3066a9df2f285edb58e20045ce7e
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-07-26 17:06:34 +0300

    no message

---
+++ Debug_Release/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Debug_Release_1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+#if DEBUG
+			Console.WriteLine("Mode=Debug");
+#else
+    Console.WriteLine("Mode=Release"); 
+#endif
+		}
+	}
+}
