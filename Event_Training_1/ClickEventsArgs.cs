2a2670306946ede5e3b044967f10c42ff4b79710
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-07-28 10:32:40 +0300

    no message

---
+++ Event_Training_1/ClickEventsArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Event_Training_1
+{
+
+    public ClickEventsArgs:EventsArgs
+    {
+        public string NameOfButton { get; set; }
+    }
+
+}
---
+++ Event_Training_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using namespace Event_Training_1;
+{
+
+
+
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var myButton = new Button();
+            myButton.OnClick += _OnClickButton();
+
+
+            myButton.Click("Yes!");
+            Console.ReadKey();
+        }
+
+        private static void _OnClickButton(object sender, ClickEventsArgs)
+        {
+            Console.WriteLine($"{e.NameOfButton}");
+
+
+        }
+    }
+}
