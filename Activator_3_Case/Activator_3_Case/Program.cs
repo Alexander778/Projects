d8d107e4730f0989890e9c8fec7165ec92f6f267
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-07-30 11:15:09 +0300

    no message

---
+++ Activator_3_Case/Activator_3_Case/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activator_3_Case
+{
+
+    public class Example
+    {
+        public void DoSmth(int x)
+        {
+            Console.WriteLine("Result: 100 / {0} = {1}",x,100/x);
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object o = Activator.CreateInstance(typeof(StringBuilder));
+            StringBuilder sb = (StringBuilder)o;
+            sb.Append("Hello, world!");
+            Example ds = new Example();
+            ds.DoSmth(5);
+            Console.WriteLine(sb);
+            Console.ReadKey();
+        }
+    }
+}
