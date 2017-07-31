8187e1efd00b738f10e0e6305a3f56dde86e9df7
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-07-31 14:23:35 +0300

    no message

---
+++ Задачка по массиву/Задачка по массиву/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задачка_по_массиву
+{
+    class Program
+    {//Дано два массива одинаковой длины (по 10 элементов). 
+     //Создайте третий массив, который будет отображать сумму первых двух массивов. 
+     //Первый элемент третьего массива равен сумме первых элементов двух первых массивов и так далее.
+        static void Main(string[] args)
+        {
+            int[] mas1 = new int[11];
+            int[] mas2 = new int[11];
+            int[] mas3 = new int[11];
+            for (int i = 1; i < 11; i++)
+            {
+                mas1[i] = i;
+                mas2[i] = i + 10;
+                mas3[i] = mas1[i] + mas2[i];
+                Console.WriteLine(mas3[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
