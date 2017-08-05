1a87e5538f96e2205e7ae6e6d583a180491826da
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-08-05 14:36:15 +0300

    no message

---
+++ Reading_from_File/Reading_from_File/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reading_from_File;
+
+namespace Reading_from_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Show.ShowText();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Reading_from_File/Reading_from_File/Show.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reading_from_File;
+
+namespace Reading_from_File
+{
+    class Show
+    {
+        public static void ShowText()
+        {
+            string file;
+            //Console.WriteLine("Введите путь к файлу");
+            file = (@"C:\Users\Alexander\Desktop\ex.txt");
+            string text = System.IO.File.ReadAllText(file);
+
+            string[] fileF = text.Split(new Char[] { ' ' });
+
+            foreach (string s in fileF)
+            {
+                
+                Console.WriteLine(s);
+                
+            }
+            
+
+
+            //Console.WriteLine(text);
+            //Console.WriteLine(file.Length);
+            Console.ReadKey();
+
+
+        }
+    }
+}
