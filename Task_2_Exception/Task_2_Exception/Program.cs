83e2a17b9e1ff3980d9ad9a2b64afbde7cce8273
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-08-12 13:23:00 +0300

    no message

---
+++ Task_2_Exception/Task_2_Exception/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your nickname. Your nickname must have not more 10 symbols");
+            try
+            {
+                string nick = Console.ReadLine();
+                if (nick.Length > 10)
+                {
+                    throw new Exception("Lenght of your nickname is so long. Create new nickname!");
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Error:" + e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
