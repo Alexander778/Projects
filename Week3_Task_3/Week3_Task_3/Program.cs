56d9e3c00263c6da3a1902fb84d8f3beecee9853
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-08-13 13:45:02 +0300

    no message

---
+++ Week3_Task_3/Week3_Task_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Week3_Task_3
+{
+    class Program
+    {
+        static void ShowInfo(object arg)
+        {
+            int counter = 0;
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine("Цифра {0}, id потока {1}", i, Thread.CurrentThread.ManagedThreadId);
+                //
+                int id = Thread.CurrentThread.ManagedThreadId;
+                List<int> IDs = new List<int>();
+                IDs.Add(id);
+                int count = IDs.Count;
+                //
+                Thread.Sleep(10);
+                counter = count;
+            }
+
+            Console.WriteLine("Статистика:");
+            Console.WriteLine("Поток, который используется: {0}",Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Количество используемых потоков:"+counter);
+            
+        }
+        static void Main(string[] args)
+        {
+            
+            ThreadPool.QueueUserWorkItem(ShowInfo);
+            Thread.Sleep(3000);
+            Console.WriteLine();
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
