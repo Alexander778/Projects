265b006ccf6fd5c856377ebd2867635bcf35c000
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-08-14 12:15:42 +0300

    no message

---
+++ Week3_Task4/Week3_Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Week3_Task4
+{
+    class Program
+    {
+        static void GetList(object arg)
+        {
+
+                Console.WriteLine("Hello!");
+            
+        }
+        static void Main(string[] args)
+        {
+
+            int Workers = 0;
+            int AsyncIO = 0;
+            ThreadPool.GetMaxThreads(out Workers,out AsyncIO);
+            ThreadPool.GetMinThreads(out Workers, out AsyncIO);
+            ThreadPool.SetMaxThreads(100, 100);
+            ThreadPool.SetMinThreads(20, 20);
+            ThreadPool.QueueUserWorkItem(GetList);
+            
+            Console.ReadKey();
+
+             
+        }
+    }
+}
