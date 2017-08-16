821dff74fa9781f26428100bc84c53d2755fd954
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-08-16 13:48:26 +0300

    no message

--- Week3_Task4/Week3_Task4/Program.cs
+++ Week3_Task4/Week3_Task4/Program.cs
@@ -22,51 +22,57 @@ namespace Week3_Task4
 
 		static void Main(string[] args)
         {
-            //
-            int a, b, c, d;
-            ThreadPool.GetMaxThreads(out a, out b);
-            ThreadPool.GetMinThreads(out c, out d);
-            Console.WriteLine("Beniging MAX: {0},{1}",a,b);
-            Console.WriteLine("Beniging MIN: {0},{1}", c, d);
-            Console.WriteLine();
-            //
-
-            //
             
-            ThreadPool.SetMaxThreads(30,20);//before
-            ThreadPool.SetMinThreads(15, 10);//before
-            ThreadPool.GetMaxThreads(out a, out b);
-            ThreadPool.GetMinThreads(out c, out d);
-            Console.WriteLine("First change MAX: {0},{1}",a,b);
-            Console.WriteLine("First change MIN: {0},{1}", c, d);
-            for (int i = 0; i < 10; i++)
+            int a_MaxThreads = 0;
+            int c_MinThreads = 0;
+ 
+            int b_PlaceHolder = 0;
+            int d_PlaceHolder = 0;
+
+            int availThreads = 0;
+            int timeOutSeconds = 5;
+
+            ThreadPool.GetMaxThreads(out a_MaxThreads, out b_PlaceHolder);
+            ThreadPool.GetMinThreads(out c_MinThreads, out d_PlaceHolder);
+            Console.WriteLine("Beniging MAX: {0},{1}", a_MaxThreads, b_PlaceHolder);
+            Console.WriteLine("Beniging MIN: {0},{1}", c_MinThreads, d_PlaceHolder);
+            Console.WriteLine();
+            while (timeOutSeconds > 0)
             {
-                ThreadPool.QueueUserWorkItem(GetList1);
+                ThreadPool.SetMaxThreads(30, 20);//before
+                ThreadPool.SetMinThreads(15, 10);//before
+                ThreadPool.GetMaxThreads(out a_MaxThreads, out b_PlaceHolder);
+                ThreadPool.GetMinThreads(out c_MinThreads, out d_PlaceHolder);
+                ThreadPool.GetAvailableThreads(out availThreads, out b_PlaceHolder);
+                Console.WriteLine("First change MAX: {0},{1}", a_MaxThreads, b_PlaceHolder);
+                Console.WriteLine("First change MIN: {0},{1}", c_MinThreads, d_PlaceHolder);
+                Console.WriteLine();
+                for (int i = 0; i < 10; i++)
+                {
+                    ThreadPool.QueueUserWorkItem(GetList1);
+                }
+                if (availThreads == a_MaxThreads) break;
             }
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                Console.WriteLine("_______________");
 
-			//
-
-			//
-			Thread.Sleep(100);
-			Console.WriteLine("_______________");
-            //
-
-            //
             ThreadPool.SetMaxThreads(20, 10); //after
-            ThreadPool.SetMinThreads(10,5);//after
-            ThreadPool.GetMaxThreads(out a, out b);
-            ThreadPool.GetMinThreads(out c, out d);
-            Console.WriteLine("Second change MAX: {0},{1}", a, b);
-            Console.WriteLine("Second change MIN: {0},{1}", c, d);
+            ThreadPool.SetMinThreads(10, 5);//after
+            ThreadPool.GetMaxThreads(out a_MaxThreads, out b_PlaceHolder);
+            ThreadPool.GetMinThreads(out c_MinThreads, out d_PlaceHolder);
+            Console.WriteLine("Second change MAX: {0},{1}", a_MaxThreads, b_PlaceHolder);
+            Console.WriteLine("Second change MIN: {0},{1}", c_MinThreads, d_PlaceHolder);
             for (int i = 0; i < 10; i++)
             {
                 ThreadPool.QueueUserWorkItem(GetList2);
             }
-            //
-            
             Console.ReadKey();
+            }
+            
+
+            
 
              
-        }
+        
     }
 }
