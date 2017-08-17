628b0b2ad36af48f1382ad60127a5699a28e6e9d
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-08-17 12:05:02 +0300

    no message

---
+++ Week3_Task5/Week3_Task5/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace Week3_Task5
+{
+    [Serializable]
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+}
