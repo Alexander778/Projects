54ebcca83225b0f0c7b579db7eda24d7665bef9d
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-08-18 14:35:22 +0300

    no message

---
+++ Week3_Task6/Week3_Task6/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace Week3_Task6
+{
+    [Serializable]
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        [NonSerialized]
+        public string SurName;
+        public Person(string name, int age,string surname)
+        {
+            Name = name;
+            Age = age;
+            SurName = surname;
+        }
+        
+    }
+
+}
---
+++ Week3_Task6/Week3_Task6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace Week3_Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Alex", 20,"Kuzmenko");
+       
+            Console.WriteLine("Object  is done for binary serialization ");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, person1);
+
+                Console.WriteLine("Object is seriazibled by BinaryFormatter");
+            }
+
+            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+            {
+                Person newperson = (Person)formatter.Deserialize(fs);
+                Console.WriteLine("Object is deserialized by BinaryFormatter");
+                Console.WriteLine("Name:{0}  -------  Age:{1} ------ Surname:{2}", newperson.Name, newperson.Age,newperson.SurName);
+            }
+            //Console.WriteLine("________________________");
+            //Console.WriteLine();
+
+            //
+            //Person person2 = new Person("Anna", 4);
+            //Console.WriteLine("Object is done for soap serialization");
+            //SoapFormatter formatter1 = new SoapFormatter();
+
+            //using (FileStream fs = new FileStream("people.soap", FileMode.OpenOrCreate))
+            //{
+            //    formatter1.Serialize(fs, person2);
+            //    Console.WriteLine("Object is serialized by SoapFormatter");
+            //}
+
+            //using (FileStream fs = new FileStream("people.soap", FileMode.OpenOrCreate))
+            //{
+            //    Person newperson1 = (Person)formatter1.Deserialize(fs);
+            //    Console.WriteLine("Object is deserialized by SoapFormatter");
+            //    Console.WriteLine("Name: {0}  -------  Age:{1}", newperson1.Name, newperson1.Age);
+
+            //}
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
