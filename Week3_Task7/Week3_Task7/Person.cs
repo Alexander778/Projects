07dc6ea2238265bed7035cfef5fb777f97071461
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-08-18 14:35:46 +0300

    no message

---
+++ Week3_Task7/Week3_Task7/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Week3_Task7
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        { }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Week3_Task7/Week3_Task7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Week3_Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Alex", 19);
+            Console.WriteLine("Object is created");
+
+            XmlSerializer formatter = new XmlSerializer(typeof(Person));
+
+            using (FileStream fs = new FileStream("people.xml", FileMode.OpenOrCreate)) 
+            {
+                formatter.Serialize(fs, person1);
+                Console.WriteLine("Object is serialized");
+            }
+
+            using (FileStream fs = new FileStream("people.xml", FileMode.OpenOrCreate)) 
+            {
+                Person newperson1 = (Person)formatter.Deserialize(fs);
+                Console.WriteLine("Object is deserialized");
+                Console.WriteLine("Name: {0} Surname: {1}",newperson1.Name,newperson1.Age);
+            }
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
