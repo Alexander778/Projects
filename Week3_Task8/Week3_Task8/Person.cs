80c97fdcb1b7aa6e8919712bb37abc0b923ac0ff
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-08-21 12:34:07 +0300

    no message

---
+++ Week3_Task8/Week3_Task8/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using Week3_Task8;
+
+namespace Week3_Task8
+{
+    [Serializable]
+    class Person:ISerializable
+    {
+        public Person()
+        {
+
+        }
+
+        private string name_value;
+        public string Name
+        {
+            get { return name_value; }
+            set { name_value = value; }
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("props", name_value, typeof(string));
+        }
+
+        public Person(SerializationInfo info, StreamingContext context)
+        {
+            name_value = (string)info.GetValue("props", typeof(string));
+        }
+
+
+
+    }
+}
---
+++ Week3_Task8/Week3_Task8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.IO;
+
+namespace Week3_Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "Persons.dat";
+            IFormatter formatter = new BinaryFormatter();
+
+            Program.SerialzeItem(fileName, formatter);
+            Program.DeserializeItem(fileName,formatter);
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+
+            
+        }
+
+        public static void SerialzeItem(string fileName, IFormatter formatter)
+        {
+            Person p1 = new Person();
+            p1.Name = "Alex";
+
+            FileStream s = new FileStream(fileName, FileMode.Create);
+            formatter.Serialize(s, p1);
+            s.Close();
+        }
+
+        public static void DeserializeItem(string fileName, IFormatter formatter)
+        {
+            FileStream s = new FileStream(fileName, FileMode.Open);
+            Person p1 = (Person)formatter.Deserialize(s);
+            Console.WriteLine(p1.Name);
+        }
+
+    }
+}
