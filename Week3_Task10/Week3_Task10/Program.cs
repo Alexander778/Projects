9c0daa854cedeaaf07cecebdad2011f994e623ec
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-08-21 13:45:57 +0300

    no message

---
+++ Week3_Task10/Week3_Task10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace Week3_Task10
+{
+    public interface IFormatter
+    {
+        SerializationBinder binder { get; set; }
+        StreamingContext context { get; set; }
+        ISurrogateSelector surrogateselector { get; set; }
+        object Deserialize(Stream serializationStream);
+        void Serialize(Stream serializationStream, object graph);
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
