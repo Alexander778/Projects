f3a6d820ed458185517fe76baa4cb0e82e3fa510
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-08-23 11:06:40 +0300

    no message

---
+++ Week3_Task11/Week3_Task11/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week3_Task11;
+
+namespace Week3_Task11
+{
+    class Person:IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ Week3_Task11/Week3_Task11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Task11
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Test();
+            Console.ReadKey();
+        }
+
+        private static void Test()
+        {
+            Person p = null;
+            try
+            {
+                p = new Person();
+            }
+            finally
+            {
+                if (p != null)
+                {
+                    p.Dispose();
+                }
+            }
+        }
+
+
+    }
+}
