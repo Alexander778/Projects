295dd31b6fc19447367c2a8149cf88d7a50d755a
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-09-02 14:07:09 +0300

    no message

---
+++ Week3_Task16/Week3_Task16/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Week3_Task16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your path");
+            string path = Console.ReadLine();
+            Console.WriteLine("");
+
+            try
+            {
+                ShowDirectory(path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        public  static void ShowDirectory(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            foreach (var item in dir.GetDirectories())
+            {
+                Console.WriteLine(item.Name);
+            }
+                
+
+        }
+    }
+}
