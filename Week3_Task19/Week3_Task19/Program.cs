2ec9767531c8119d73e5e68ddcfda4fa3841ed3c
Alexander778 <aleksandr.kuzmenko778@gmail.com>
2017-09-04 14:04:10 +0300

    no message

---
+++ Week3_Task19/Week3_Task19/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Week3_Task19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any button");
+            
+            for (int i = 0; i < 10; i++)
+            {
+                var a = Console.ReadKey();
+                if ( a!= null)
+                {
+                    Console.Beep();
+                }
+                else
+                {
+
+                }
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
