2e4aa8f6bddfd8e91c56c556b6988d632a83082b
Alex Kuzmenko <aleksandr.kuzmenko778@gmail.com>
2017-09-19 19:00:20 +0300

    no message

---
+++ Week3_Task20/Week3_Task20/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace Week3_Task20
+{
+
+    public class Win32
+    {
+        [DllImport("user32.dll")]
+        public static extern void LockWorkStation();
+    }
+
+    
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+           
+        
+        
+        Console.WriteLine("Computer Locker");
+            Console.WriteLine("Do you want to lock your computer?Y/N");
+            string decision = Console.ReadLine();
+            switch(decision)
+            {
+                case "Y":
+                    Win32.LockWorkStation();
+                    break;
+                case "N":
+                    Environment.Exit(0);
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
